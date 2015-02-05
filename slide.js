1a8d19de6854538281dfad5b8662c869c5a51a4d
菅崎葵 <sugasakiaoi@sugasakiaoi-no-MacBook-Pro.local>
2015-02-05 13:34:00 +0900

    file upload

---
+++ slide.js
@@ -0,0 +1,15 @@
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
+	
+	
+	$(function(){
+	    var setImg = '#slide';
+	    var fadeSpeed = 1500;
+	    var switchDelay = 5000;
+	 
+	    $(setImg).children('img').css({opacity:'0'});
+	    $(setImg + ' img:first').stop().animate({opacity:'1',zIndex:'20'},fadeSpeed);
+	 
+	    setInterval(function(){
+	        $(setImg + ' :first-child').animate({opacity:'0'},fadeSpeed).next('img').animate({opacity:'1'},fadeSpeed).end().appendTo(setImg);
+	    },switchDelay);
+	});
